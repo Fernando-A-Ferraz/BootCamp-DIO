fa8dd3c68787bd56ca039540f62f8476c7c5e014
Fernando <fferrazth@gmail.com>
2024-01-16 19:28:38 -0300

    Propriedades, Métodos e Construtores com C#

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Fernando",sobrenome: "Ferraz");
+ Pessoa p2 = new Pessoa(nome: "Sofia",sobrenome: "Ferraz");
+ 
+
+ Curso cursoDeIngles = new Curso();
+ cursoDeIngles.Nome = "ingles";
+ cursoDeIngles.Alunos = new List<Pessoa>();
+
+ cursoDeIngles.AdicionarAluno(p1);
+ cursoDeIngles.AdicionarAluno(p2);
+ cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Fernando";
+// p1.Idade = 35;
+// p1.Sobrenome = "Ferraz";
+// p1.Apresentar();
