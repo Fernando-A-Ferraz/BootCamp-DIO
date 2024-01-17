d353de2273f38d7bb23c04dbc9978516a14fb3f3
Fernando <fferrazth@gmail.com>
2024-01-17 09:44:19 -0300

    Exceções e Coleções com C#

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -5,16 +5,198 @@ using System.Threading.Tasks;
 using ExemploExplorando.Models;
 using System.Globalization;
 
-string dataString = "2024-01-17 00:48";
-DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
-if (sucesso)
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach(var item in estados)
 {
-    Console.WriteLine($"Converção com sucesso! Data: {data}");
+    Console.WriteLine($"Chave:{item.key}, Valor: {item.value}");
+
+} Console.WriteLine("__________________");
+
+estados.remove("BA");
+estados["SP"] = "São Paulo - valor alterado";
+
+foreach(var item in estados)
+{
+    Console.WriteLine($"Chave:{item.key}, Valor: {item.value}");
+
+}
+string chave = "BA";
+Console.WriteLine($"Verificando o elemento:{chave}");
+if(estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
 }
 else
 {
-    Console.WriteLine($"{dataString} não é uma data válida");
+    Console.WriteLine($"Valor não existe. É seguro adicionar chave: {chave}");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Stack<int> pilha = new Stack<int>();
+
+//  pilha.Push(4);
+//  pilha.Push(6);
+//  pilha.Push(8);
+//  pilha.Push(10);
+
+//  foreach(int item in pilha)
+//  {
+//     Console.WriteLine(item);
+//  }
+//  Console.WriteLine($"Removendo o elmento do topo: {pilha.pop()}");
+// pilha.Push(20);
+
+//  foreach(int item in pilha)
+//  {
+//     Console.WriteLine(item);
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Queue<int> fila = Queue<int>();
+
+//  fila.Enqueue(2);
+//  fila.Enqueue(4);
+//  fila.Enqueue(6);
+//  fila.Enqueue(8);
+
+//  foreach(int item in fila)
+//  {
+//     Console.WriteLine(item);
+//  }
+//  Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+//  foreach(int item in fila)
+//  {
+//     Console.WriteLine(item);
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//new ExemploExecao().Metodo1();
+
+
+
+
+
+
+
+// try
+// {
+// string[] linhas = File.ReadAllLines("Arquivos/aquivoLeitura.txt");
+
+// foreach(string Linha in linhas)
+//  {
+//     Console.WriteLine(linha);
+//  }
+// }
+
+// catch(FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado {ex.Message}");
+// }
+//  catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica.{ex.Message}");
+// }
+// finally
+// {
+// Console.WriteLine("Seu programa chegou até aqui");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string dataString = "2024-01-17 00:48";
+// DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+// if (sucesso)
+// {
+//     Console.WriteLine($"Converção com sucesso! Data: {data}");
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida");
+// }
 
 
 
